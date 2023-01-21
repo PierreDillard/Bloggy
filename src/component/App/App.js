b184e4d68596b84b22e228ca83a6796e1b18c193
Pierre Dillard <7gaspard77@gmail.com>
2023-01-21 11:38:31 +0100

    add react-router-dom et add in index.js et App.js

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,13 +1,26 @@
-
+import { Routes, Route,Link } from 'react-router-dom';
 import './styles.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-    
-  
-    </div>
-  );
+    <Routes>
+      <div>
+        <Link to='/news'>News</Link>
+        <Link to='/art-galery'>ArtGalery</Link>
+        <Link to='/internship'>Internship</Link>
+        <Link to='/video'>Video</Link>
+        <Link to='/contact'>Contact</Link>
+        <Route path='/news' element={(<News />)} />
+        <Route path='/art-galery' element={(<ArtGallery />)} />
+        <Route path='/internship' element={(<Internship />)} />
+        <Route path='/video' element={(<Videos />)} />
+        <Route path='/contact' element={(<Contact />)} />
+      </div>
+    </Routes>
+  )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from '../src/component/App/App';c
+import App from '../src/component/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider>
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+  </BrowserRouter>
+ 
+
+
+  </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
