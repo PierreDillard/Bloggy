cd6295b0091525efc5dd8505f556dd27560d1e1a
Pierre Dillard <7gaspard77@gmail.com>
2023-01-21 17:17:52 +0100

    refactor App.jsx

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -7,11 +7,7 @@ function App() {
   return (
     <Routes>
       <div>
-        <Link to='/news'>News</Link>
-        <Link to='/art-galery'>ArtGalery</Link>
-        <Link to='/internship'>Internship</Link>
-        <Link to='/video'>Video</Link>
-        <Link to='/contact'>Contact</Link>
+   
         <Route path='/news' element={(<News />)} />
         <Route path='/art-galery' element={(<ArtGallery />)} />
         <Route path='/internship' element={(<Internship />)} />
