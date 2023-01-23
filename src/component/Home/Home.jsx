fce009b6616f68b5e2f45b8f43d840b37813fd44
Pierre Dillard <7gaspard77@gmail.com>
2023-01-23 14:15:30 +0100

    refactor components

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
  function Home() {
   return (
@@ -6,9 +7,11 @@ import React from 'react'
     <h1>
         BloGGy
     </h1>
+    <Link to='/login'>
     <button>
         Connexion
     </button>
+    </Link>
       
     </div>
   )
