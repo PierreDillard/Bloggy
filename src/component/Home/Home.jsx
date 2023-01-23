76c7c9624eac988b724ee1ff3f9d46f998fae204
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-23 23:12:44 +0100

    integration Home.css and Login.css

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -4,15 +4,16 @@ import './Home.css';
 
  function Home() {
   return (
-    <div className='Home-container'>
-      <h1 className='Home-title'>
-        BloGGy
-      </h1>
-      <Link to='/login'>
-        <button className='Home-button-connexion'>
-          Connexion
-        </button>
-      </Link>
+    <div className='Home__container'>
+      <div className='Home__banner'>
+        <h1 className='Home__title'>BloGGy</h1>
+        <Link to='/login'>
+          <button className='Home__button'>
+          <span className='Home__button-content'>Connexion</span>
+          </button>
+        </Link>
+      </div>
+      <p className='Home__slogan'>Le blog fait pour toi</p>
     </div>
   )
 }
