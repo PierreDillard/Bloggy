ac34c41ce8a11e0ae9b60f06bba75ca14835f567
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-24 17:31:34 +0100

    add Menu.jsx and Menu.css

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -4,6 +4,5 @@ import './Button.css';
 export default function Button() {
   return (
     
-    
   )
 }
--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -4,54 +4,58 @@ import './Menu.css';
 
 export default function Menu() {
   return (
-    <div>
-        <div className='menu'>
-        {/* Home */}
-        <Link to='/'>
-            <button className='home__button'>
-            <span className='home__button-content'>Accueil</span>
-            </button>
-        </Link>
-        {/* News */}
-        <Link to='/news'>
-            <button className='home__button'>
-            <span className='home__button-content'>Actualités</span>
-            </button>
-        </Link>
-        {/* Videos */}
-        <Link to='/video'>
-            <button className='home__button'>
-            <span className='home__button-content'>Actualités</span>
-            </button>
-        </Link>
-        {/* Art gallery */}
-        <Link to='/art-gallery'>
-            <button className='home__button'>
-            <span className='home__button-content'>Actualités</span>
-            </button>
-        </Link>
-        {/* Internships */}
-        <Link to='/internship'>
-            <button className='home__button'>
-            <span className='home__button-content'>Actualités</span>
-            </button>
-        </Link>
-        {/* Contact */}
-        <Link to='/contact'>
-            <button className='home__button'>
-            <span className='home__button-content'>Actualités</span>
-            </button>
-        </Link>
-    </div>
-    <div>
-        {/* Disonnect */}
-        <Link to='/contact'>
-            <button className='home__button'>
-            <span className='home__button-content'>Déconnexion</span>
-            </button>
-        </Link>
-    </div>
-    </div>
+    <React.Fragment>
+        <div className='menu__container'>
+            <div className='menu__connect'>
+                {/* Home */}
+                <Link to='/'>
+                    <button className='menu__button menu__button--home'>
+                        <span className='menu__button-content'>Accueil</span>
+                    </button>
+                </Link>
+                {/* News */}
+                <Link to='/news'>
+                    <button className='menu__button'>
+                        <span className='menu__button-content'>Actualités</span>
+                    </button>
+                </Link>
+                {/* Videos */}
+                <Link to='/video'>
+                    <button className='menu__button'>
+                        <span className='menu__button-content'>Vidéos</span>
+                    </button>
+                </Link>
+                {/* Art gallery */}
+                <Link to='/art-gallery'>
+                    <button className='menu__button'>
+                        <span className='menu__button-content'>Galerie d'art</span>
+                    </button>
+                </Link>
+                {/* Internships */}
+                <Link to='/internship'>
+                    <button className='menu__button'>
+                        <span className='menu__button-content'>Stages</span>
+                    </button>
+                </Link>
+                {/* Contact */}
+                <Link to='/'>
+                    <button className='menu__button'>
+                        <span className='menu__button-content'>Contact</span>
+                    </button>
+                </Link>
+            </div>
+            <div className='menu__disconnect'>
+                {/* Disonnect */}
+                <Link to='/'>
+                    <button className='menu__button'>
+                        <span className='menu__greeting'>Bonjour !</span>
+                        <span className='menu__button-content'>Déconnexion</span>
+                    </button>
+                </Link>
+            </div>
+        </div>
+        
+    </React.Fragment>
     
   )
 }
