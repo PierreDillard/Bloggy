253f601cf4cceed66ab5f7209672e2e129726b99
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-25 12:35:22 +0100

    refactor Home.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Header.css';
 import Menu from '../Menu/Menu';
+import imgLogo from "../../assets/logo_BloGGy_white.svg";
+// import { ReactReduxContext } from 'react-redux';
 
 
 export default function Header() {
   return (
-    // logo
-    <Menu />
-    
+    <React.Fragment>
+      <img className="home__logo" src={imgLogo} alt="Logo de BloGGy" />
+      <Menu />
+    </React.Fragment>
   )
 }
 
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -9,30 +9,24 @@ export default function Home() {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   return (
     <div className="home__container">
-      <div className="home__banner">
-        <img className="home__logo" src={imgLogo} alt="Logo de BloGGy" />
 
-        {/* L'utilisateur n'est pas connecté, on affiche "connexion", sinon on affiche rien  */}
-        {!isLoggedIn ? (
+      {/* L'utilisateur n'est pas connecté, on affiche "connexion", sinon on n'affiche rien */}
+      {!isLoggedIn ? (
+        <React.Fragment>
+          <div className="home__banner">
+          <img className="home__logo" src={imgLogo} alt="Logo de BloGGy" />
           <Link to="/login">
             <button className="home__button home__button--connexion">
               <span className="home__button-content">Connexion</span>
             </button>
           </Link>
-        ) : null}
-      </div>
-<<<<<<< HEAD
-        {/*     Si l'utilisateur est connecté, on affiche le menu s'il n'est pas connecté on affiche rien */}
-        {isLoggedIn ? <Menu /> : null}
-      <p className='home__slogan'>Le blog fait pour toi</p>
-=======
-      {/*     Si l'utilisateur est connecté, on affiche le menu s'il n'est pas connecté on affiche rien */}
-      {isLoggedIn ? <Header /> : null}
-
-      {!isLoggedIn ? (
+        </div>
         <p className="home__slogan">Le blog fait pour toi</p>
+        </React.Fragment>
       ) : null}
->>>>>>> 3ce5ef881f2dc872bedfd4160f7af198e1ce0b2d
+
+      {/* Si l'utilisateur est connecté, on affiche le menu */}
+      {isLoggedIn ? <Header /> : null}
     </div>
   );
 }
