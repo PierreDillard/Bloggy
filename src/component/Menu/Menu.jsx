461343ac8b73e179ed5f100050228e3077a5ea9b
Pierre Dillard <7gaspard77@gmail.com>
2023-01-26 10:48:51 +0100

    add active page Home,News..

--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link , useNavigate} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import './Menu.css';
+
 import { actionlogout } from '../../actions/user';
+import { useLocation } from 'react-router-dom';
+import './Menu.css';
 
 export default function Menu() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const location = useLocation();
 
 /* Cette fonction, envoie l'actionlogout qui sera récupéré par le reducer, pour supprimer, l'Email, le token et passer isLoggedIn à false */
 
@@ -20,36 +23,40 @@ export default function Menu() {
             <div className='menu__connect'>
                 {/* Home */}
                 <Link to='/'>
-                    <button className='menu__button menu__button--current'>
+                    <button className={`menu__button ${location.pathname === '/' ? 'menu__button--current' : ''}`}>
                         <span className='menu__button-content'>Accueil</span>
                     </button>
                 </Link>
                 {/* News */}
                 <Link to='/news'>
-                    <button className='menu__button'>
+                <button className={`menu__button ${location.pathname === '/news' ? 'menu__button--current' : ''}`}>
                         <span className='menu__button-content'>Actualités</span>
                     </button>
                 </Link>
                 {/* Videos */}
                 <Link to='/video'>
-                    <button className='menu__button'>
+                <button className={`menu__button ${location.pathname === '/video' ? 'menu__button--current' : ''}`}>
+                   
                         <span className='menu__button-content'>Vidéos</span>
                     </button>
                 </Link>
                 {/* Art gallery */}
                 <Link to='/art-gallery'>
-                    <button className='menu__button'>
+                <button className={`menu__button ${location.pathname === '/art-gallery' ? 'menu__button--current' : ''}`}>
+                   
                         <span className='menu__button-content'>Galerie d'art</span>
                     </button>
                 </Link>
                 {/* Internships */}
                 <Link to='/internship'>
-                    <button className='menu__button'>
+                <button className={`menu__button ${location.pathname === '/internship' ? 'menu__button--current' : ''}`}>
+                   
                         <span className='menu__button-content'>Stages</span>
                     </button>
                 </Link>
                 {/* Contact */}
-                <Link to='/'>
+            {/*     to do créer une page contact */}
+            <Link to='/contact'>
                     <button className='menu__button'>
                         <span className='menu__button-content'>Contact</span>
                     </button>
@@ -71,5 +78,3 @@ export default function Menu() {
     
   )
 }
-
-
