930cc7436b3879201db90701a705ecd63bd5a98b
Pierre Dillard <7gaspard77@gmail.com>
2023-01-27 15:49:46 +0100

    card component add, img)

--- src/component/ArtGallery/ArtGallery.jsx
+++ src/component/ArtGallery/ArtGallery.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import Header from "../Header/Header";
+import Card from "../Card/Card";
 import "./ArtGallery.css";
+
+const data = [
+  { id: 1, title: 'Actualités' },
+  { id: 2, title: 'Sport' },
+  { id: 3, title: 'Théâtre' },
+];
 
 export default function ArtGallery() {
   return (
     <React.Fragment>
       <div className="art-gallery__container">
         <Header />
-        <div className="card__container">
-          <div className="card">
+        <div className="art-gallery__card__container">
+        {data.map((item) => (
+          <Card key={item.id} title={item.title} />
 
-          </div>
+        ))}
+       
+        
+        
+
         </div>
+        
+     
       </div>
     </React.Fragment>
   );
--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import Button from'../Button/Button';
+import image from '../../assets/home.webp'
 import './Card.css';
 
 
 
 
-export default function Card() {
+export default function Card(props) {
+
+  console.log(props);
   
   return (
-    <div className='card-container'>
-       <div className='card-header'>
+    <div className='card__container'>
+       <div className='card__header'>
+       <div className="card__bouton__container">
         <button className='card__button'>Modifier</button>
         <button className='card__button'>Supprimer</button>
-        <span className='card__tag'>Actualites</span>
+
+       </div>
+        <span className='card__tag'>{props.title}</span>
       </div>
+      <img src={image} className='card__image'/>
+      
     
      <p className='card__description'>here are many variations of passages of Lorem Ipsum available, but the majority</p>
       <p className='card__comment'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
