d805135f0a7212b820f7805e0d4c7365f8d456cd
Pierre Dillard <7gaspard77@gmail.com>
2023-01-27 18:18:56 +0100

    border description

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -11,20 +11,32 @@ export default function Card(props) {
   console.log(props);
   
   return (
+    <React.Fragment>
+
     <div className='card__container'>
+    
+
+   
        <div className='card__header'>
        <div className="card__bouton__container">
         <button className='card__button'>Modifier</button>
         <button className='card__button'>Supprimer</button>
+        </div>
+        <span className='card__tag'>{props.title}</span>
 
        </div>
-        <span className='card__tag'>{props.title}</span>
-      </div>
+       <div className="card__content">
+        
       <img src={image} className='card__image'/>
       
     
      <p className='card__description'>here are many variations of passages of Lorem Ipsum available, but the majority</p>
       <p className='card__comment'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+     
+      </div>
     </div>
+    </React.Fragment>
+  
+    
   )
 }
