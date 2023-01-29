a9584d09c8b84d48d1465c0d3013a9f8dcc64c2c
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-29 18:38:38 +0100

    Add component Papers

---
+++ src/component/Papers/Papers.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Papers.css";
+
+export default function Papers() {
+  return (
+    <React.Fragment>
+      <div className='papers__container'>
+        
+        
+      </div>
+    </React.Fragment>
+  )
+}
