fb56a8f5d22fe600e6664a36a50829b3c4728546
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-29 18:33:30 +0100

    Component Dico finished, API link to be changed

--- src/component/News/News.jsx
+++ src/component/News/News.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from "../Header/Header";
+import Dico from "../Dico/Dico";
 import Modale from '../Modale/Modale';
 import "./News.css";
 
@@ -8,7 +9,7 @@ export default function News() {
     <React.Fragment>
       <div className='news__container'>
         <Header />
-      
+        
       </div>
     </React.Fragment>
   )
