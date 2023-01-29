4f0681b289beafd6b0837bd2aa682c772e618fe3
sophiecavadini <sophie.cavadini@gmail.com>
2023-01-29 16:16:59 +0100

    Add Dico.jsx, Dico.css, roads in App.jsx

---
+++ src/component/Dico/Dico.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Dico.css';
+
+export default function Dico() {
+
+
+
+    return (
+
+        <React.Fragment>
+            coucou
+        </React.Fragment>
+
+    )
+
+}
+
+
