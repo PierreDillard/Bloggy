ef671eda0b0764e683bd3e6f7265551b87b64e58
Pierre Dillard <7gaspard77@gmail.com>
2023-02-02 20:53:12 +0100

    modify img on Card's Button

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,64 +1,41 @@
-import React, { memo, useState} from 'react';
+import React, { memo, useState } from 'react';
+import EditionImage from '../EditionImage/EditionImage';
 import Button from'../Button/Button';
 import PropTypes from 'prop-types';
-import image from '../../assets/home.webp';
-import EditionImage from '../EditionImage/EditionImage';
 import Comment from '../Comment/Comment';
+import './Card.css';
 import { Card } from 'react-bootstrap';
 
-import './Card.css';
-import '../Comment/Comment'
+export default memo(function Card({ id, ...props }) {
+  const [showFileInput, setShowFileInput] = useState(false);
 
-
-/* On utilise React memo, pour optimiser la performance de Card, on ne ré rendra Card, que si les props ont changé */
-
-export default memo(function Card({id,...props}) {
-
-  const [showModal, setShowModal] = useState(false);
-
-  const handleClick = () => {
-    setShowModal(true);
-  };
-
-  
-console.log(props);
-  
   return (
-
     <React.Fragment>
-
-      <div className='card__container'>
-    
-        <div className='card__header'>
-
+      <div className="card__container">
+        <div className="card__header">
           <div className="card__bouton-container">
-            <button className='card__button card__button--modify' onClick={handleClick}>Modifier</button>
-            <button className='card__button card__button--cancel'>Supprimer</button>
+            <button
+              className="card__button card__button--modify"
+              onClick={() => setShowFileInput(!showFileInput)}
+            >
+              Modifier
+            </button>
+            <button className="card__button card__button--cancel">
+              Supprimer
+            </button>
           </div>
-
-          <span className='card__tag'>{props.title}</span>
-
         </div>
-        
         <div className="card__content">
-
-        {showModal && <EditionImage showModal={showModal} setShowModal={setShowModal} />}
-
-          <EditionImage className="edition__image-container" />
+          <EditionImage showFileInput={showFileInput} />
           <Comment 
           key={props.id}>
 
             
           </Comment> 
-
         </div>
-
       </div>
-
     </React.Fragment>
-    
-  )
-  
+  );
 });
 
 /* On utilise propTypes, pour "typer" les props, en cas de Bug, on sait que la props attendu doit être une string */
--- src/component/EditionImage/EditionImage.jsx
+++ src/component/EditionImage/EditionImage.jsx
@@ -2,60 +2,20 @@ import React, { useState } from 'react';
 import image from '../../assets/home.webp';
 import './EditionImage.css';
 
-export default function EditionImage({showModal, setShowModal}) {
- 
+export default function EditionImage({ showFileInput , setShowFileInput}) {
   const [imageUrl, setImageUrl] = useState(image);
-  const [showFileInput, setShowFileInput] = useState(false);
-  const [description, setDescription] = useState(
-    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
-  );
-  const [showDescriptionInput, setShowDescriptionInput] = useState(false);
-
-  /*   const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = downloadUrl;
-    link.download = 'image.webp';
-    link.click();
-  }; */
 
   const handleFileInput = (event) => {
     setImageUrl(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleDescriptionInput = (event) => {
-    setDescription(event.target.value);
   };
 
   return (
     <React.Fragment>
       <div className="edition__image__container">
         <img src={imageUrl} className="edition__image" />
-        <span className="image__author">
-          Par Sam
-        </span>
-        
       </div>
-      {showFileInput && (
-        <input type="file" onChange={handleFileInput} />
-      )}
-      <button onClick={() => setShowFileInput(!showFileInput)}
-      className="button__supprimer">Modifier</button>
-      <button onClick={() => setImageUrl(null)}>Supprimer</button>
-     {/*  <button onClick={handleDownload}>Télécharger une image</button> */}
-      {showDescriptionInput ? (
-        <div className="description__input__container">
-          <input
-            type="text"
-            value={description}
-            onChange={handleDescriptionInput}
-          />
-          <button onClick={() => setDescription('')}
-          className="button__supprimer">Supprimer</button>
-        </div>
-      ) : (
-        <p className="description">{description}</p>
-      )}
-      <button onClick={() => setShowDescriptionInput(!showDescriptionInput)}>
+      {showFileInput && <input type="file" onChange={handleFileInput} />}
+      <button onClick={() => setShowFileInput(!showFileInput)}>
         Modifier
       </button>
     </React.Fragment>
