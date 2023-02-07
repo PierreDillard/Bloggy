0f7770c03b879e6697721c586d702dfec61484ab
sophiecavadini <sophie.cavadini@gmail.com>
2023-02-07 17:29:21 +0100

    Clean CreateCard.jsx

--- src/component/CreateCard/CreateCard.jsx
+++ src/component/CreateCard/CreateCard.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import axios from 'axios';
-import api from "../../api";
-
 import './CreateCard.css';
-
-
 
 
 export default function  CreateCard() {
 
-  
-
-
-  
   
   const [description, setDescription] = useState("");
   const [uploaded_file, setUploaded_file] = useState(null);
@@ -35,73 +26,83 @@ export default function  CreateCard() {
     try {
       const response = await axios.post("http://localhost:5000/api/card/addCard", formData,  { withCredentials: true });
       console.log(response.data);
+
     } catch (error) {
       console.error(error);
     }
+
   };
 
   return (
+
     <div className="form__container">
 
-   
-    <form onSubmit={handleSubmit}>
-      <label>
-        Description:
-        <input
-          type="text"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          name="description"
-        />
-      </label>
-      <br />
-      <label>
-        Image:
-        <input 
-        type="file"
-         onChange={(event) => setUploaded_file(event.target.files[0])}
-         name="file" />
-      </label>
-      <br />
-      <label>
-        Type:
-        <input
-          type="text"
-          value={type}
-          onChange={(event) => setType(event.target.value)}
-          name="type"
-        />
-      </label>
-      <label>
-  Type de média:
-  <select value={type} onChange={(event) => setType(event.target.value)}>
-    <option value="news">News</option>
-    <option value="art">Art</option>
-    <option value="video">Vidéo</option>
-  </select>
-</label>
-<br />
-<label>
-  Url:
-  <input
-    type="text"
-    value={url}
-    onChange={(event) => setUrl(event.target.value)}
-    name="url"
-  />
-</label>
-      <br />
-      <label>
-        Member ID:
-        <input
-          type="number"
-          value={member_id}
-          onChange={(event) => setMemberId(event.target.value)}
-          name="member_id"
-        />
-      </label>
-      <button type="submit">Envoyer</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          Description:
+          <input
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            name="description"
+          />
+        </label>
+
+        <label>
+          Image:
+          <input 
+            type="file"
+            onChange={(event) => setUploaded_file(event.target.files[0])}
+            name="file" 
+          />
+        </label>
+
+        <label>
+          Type:
+          <input
+            type="text"
+            value={type}
+            onChange={(event) => setType(event.target.value)}
+            name="type"
+          />
+        </label>
+
+        <label>
+          Type de média:
+          <select value={type} onChange={(event) => setType(event.target.value)}>
+            <option value="news">News</option>
+            <option value="art">Art</option>
+            <option value="video">Vidéo</option>
+          </select>
+        </label>
+
+        <label>
+          Url:
+          <input
+            type="text"
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+            name="url"
+          />
+        </label>
+
+        <label>
+          Member ID:
+          <input
+            type="number"
+            value={member_id}
+            onChange={(event) => setMemberId(event.target.value)}
+            name="member_id"
+          />
+        </label>
+
+         <button type="submit">Envoyer</button>
+
+      </form>
+
     </div>
+
   );
+  
 };
